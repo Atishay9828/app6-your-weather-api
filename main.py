6b1410df1d54dc3e04b9f0108de52b5458ac8420
AJ <149464895+Atishay9828@users.noreply.github.com>
2025-07-12 16:44:07 +0530

    temp and date update dynamically

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template as rt
+import pandas as pd
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return rt("home.html")
-@app.route("/about/")
-def about():
-    return rt("about.html")
-
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+    filename= "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename,skiprows= 20, parse_dates= ["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {
+        "temperature" : temperature ,
+        "date" : date ,
+        "station" : station 
+    }
 
 app.run(debug=True)
