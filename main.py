6e9eb57a9d92477bd52fa7ff78ada9d9aac70e51
AJ <149464895+Atishay9828@users.noreply.github.com>
2025-07-12 17:36:13 +0530

    add url endpoint for yearly , stations speciifc

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ stations = stations[["STAID" , "STANAME                                 "]]
 def home():
     return rt("home.html" , data= stations.to_html())
 @app.route("/api/v1/<station>/<date>")
-def about(station,date):
+def station_date(station,date):
     filename= "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename,skiprows= 20, parse_dates= ["    DATE"])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
@@ -18,5 +18,19 @@ def about(station,date):
         "date" : date ,
         "station" : station 
     }
+@app.route("/api/v1/<station>")
+def station(station):
+    filename= "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename,skiprows= 20, parse_dates= ["    DATE"])
+    return df.to_dict(orient= "records")
+
+@app.route("/api/v1/year/<station>/<year>")
+def yearly(station,year):
+    filename= "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename,skiprows= 20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))]
+
+    return result.to_dict(orient= "records")
 
 app.run(debug=True)
